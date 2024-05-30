bee3f3e55ed8636ea5479b60c3172eae94586177
Alba <100930901+albadelatorres@users.noreply.github.com>
2024-05-30 23:18:13 +0200

    fin practica

--- crearInformeTest.js
+++ crearInformeTest.js
@@ -16,38 +16,67 @@ export const options = {
         checks: ["rate==1.0"]
     }
 }
-
-export default async function(){
+export default async function () {
     const page = browser.newPage();
 
     try {
 
         await page.goto('http://localhost:4200/');
 
-        check(page, {
-            'Botón Predecir presente': p => p.locator('button[name="predict"]').isVisible(),
-        });
-    
-        if (await page.locator('button[name="predict"]').isVisible()) {
-            await page.locator('button[name="predict"]').click();
+        page.locator('input[name="nombre"]').type('Medico1');
+        page.locator('input[name="DNI"]').type('11111111X');
 
-            await sleep(1);
-        }
+        const submitButton = page.locator('button[name="login"]');
+        await Promise.all([
+            page.waitForNavigation(),
+            submitButton.click()
+        ]);
 
         check(page, {
-            'Texto Probabilidad de cáncer presente': async p => {
-                const spanText = await p.locator('div.result span').textContent();
-                return spanText.includes('Probabilidad de cáncer:');
-            }
+            'header': p => p.locator('h2').textContent() == 'Listado de pacientes',
         });
-    
+
+        page.waitForSelector("table tbody");
+        const firstRow = page.$$("table tbody tr")[0].$('td[name= "nombre"]');
+        await Promise.all([
+            page.waitForNavigation(),
+            firstRow.click()
+        ]);
+
+        sleep(3);
+
+        const viewButton = page.$$("table tbody tr")[0].$('button[name="view"]');
+        await Promise.all([
+            page.waitForNavigation(),
+            viewButton.click()
+        ]);
+
+        sleep(3);
+
+        page.waitForSelector('button[name="add"]');
+
+        const addButton = page.locator('button[name="add"]');
+        await Promise.all([
+            page.waitForNavigation(),
+            addButton.click()
+        ]);
+
+        page.waitForSelector("textarea")
+
+        page.locator('textarea').type("Este es el informe médico");
+        const saveButton = page.locator('button[name="save"]');
+        await Promise.all([
+            page.waitForNavigation(),
+            saveButton.click()
+        ]);
+        
+        page.waitForSelector('.info-value');
+
+        // Si existe el título informe de la imagen, es que el informe se ha creado correctamente
         check(page, {
-            'Campo Informe médico presente': p => p.locator('textarea[matInput]').isVisible(),
+            'H1 del informe está presente': p => p.locator('h1[class="underline"]').textContent().includes("Informe de la imagen")
         });
-    
-        check(page, {
-            'Botón Guardar presente': p => p.locator('button[name="save"]').isVisible(),
-        });
+
 
     } finally {
         page.close();
--- realizarPrediccionTest.js
+++ realizarPrediccionTest.js
@@ -16,45 +16,56 @@ export const options = {
         checks: ["rate==1.0"]
     }
 }
-export default async function(){
-    const page = context.newPage();
+
+export default async function () {
+    const page = browser.newPage();
 
     try {
+        await page.goto('http://localhost:4200/');
 
-        // acceder a la web
-        await page.goto('http://localhost:4200/',{ waitUntil: 'networkidle' });
+        page.locator('input[name="nombre"]').type('Medico1');
+        page.locator('input[name="DNI"]').type('11111111X');
 
-        check(page, {
-            'Paciente nombre presente': p => p.locator('h1').textContent() == ('Paciente: Juan'),
-        });
-
-        check(page, {
-            'Botón Predecir presente': p => p.locator('button[name="predict"]').exists(),
-        });
-
-        check(page, {
-            'Botón Añadir informe presente': p => p.locator('button[name="add"]').exists(),
-        });
-
-        const submitButton = page.locator('button[name="predict"]');
+        const submitButton = page.locator('button[name="login"]');
         await Promise.all([
             page.waitForNavigation(),
             submitButton.click()
         ]);
 
         check(page, {
-            'La prediccion se ha realizado correctamente': p => p.locator('span').textContent() == 'Probabilidad de cáncer:',
+            'header': p => p.locator('h2').textContent() == 'Listado de pacientes',
         });
 
-        const addButton = page.locator('button[name="add"]');
+        sleep(3);
+        page.waitForSelector("table tbody");
+        let firstRow = page.$$("table tbody tr")[0].$('td[name= "nombre"]');
         await Promise.all([
-            page.waitForNavigation(),
-            addButton.click()
+            page.waitForNavigation(), 
+            firstRow.click()
         ]);
 
+        sleep(3);
+        page.waitForSelector("table tbody");
+        let viewButton = page.$$("table tbody tr")[0].$('button[name="view"]');
+        await Promise.all([
+            page.waitForNavigation({waitUntil: 'networkidle'}),
+            viewButton.click()
+        ]);
+
+        sleep(3);
+        page.waitForSelector('button[name="predict"]');
+
+        let predictButton = page.locator('button[name="predict"]')
+        await Promise.all([
+            page.waitForTimeout(3),
+            predictButton.click()
+        ]);
+
+        sleep(3);
         check(page, {
-            'nos redirige a crear informe': p => p.locator('mat-label').textContent() == 'Informe médico',
+            'Resultado de la predicción': p => p.$('span[name="predict"]').textContent().includes("Probabilidad de cáncer")
         });
+
 
     } finally {
         page.close();
